aba814bf2fdc29b7077f623507af155349bbdec9
James Smith <codefiend@gmail.com>
2014-08-31 17:45:04 -0700

    Updated file header comment

--- sccec_stair_piano.ino
+++ sccec_stair_piano.ino
@@ -1,28 +1,7 @@
 /*
- * speaker_pcm
- *
- * Plays 8-bit PCM audio on pin 11 using pulse-width modulation (PWM).
- * For Arduino with Atmega168 at 16 MHz.
- *
- * Uses two timers. The first changes the sample value 8000 times a second.
- * The second holds pin 11 high for 0-255 ticks out of a 256-tick cycle,
- * depending on sample value. The second timer repeats 62500 times per second
- * (16000000 / 256), much faster than the playback rate (8000 Hz), so
- * it almost sounds halfway decent, just really quiet on a PC speaker.
- *
- * Takes over Timer 1 (16-bit) for the 8000 Hz timer. This breaks PWM
- * (analogWrite()) for Arduino pins 9 and 10. Takes Timer 2 (8-bit)
- * for the pulse width modulation, breaking PWM for pins 11 & 3.
- *
- * References:
- *     http://www.uchobby.com/index.php/2007/11/11/arduino-sound-part-1/
- *     http://www.atmel.com/dyn/resources/prod_documents/doc2542.pdf
- *     http://www.evilmadscientist.com/article.php/avrdac
- *     http://gonium.net/md/2006/12/27/i-will-think-before-i-code/
- *     http://fly.cc.fer.hr/GDM/articles/sndmus/speaker2.html
- *     http://www.gamedev.net/reference/articles/article442.asp
- *
- * Michael Smith <michael@hurts.ca>
+ * Sierra College Computer Engineering Club
+ * Staircase Piano Project
+ * author: James Smith
  */
 
 #include <stdint.h>
