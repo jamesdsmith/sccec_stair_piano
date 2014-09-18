c98d14150012d31d45386c64f5b274b5d7ad36d5
James Smith <codefiend@gmail.com>
2014-09-18 02:11:12 -0700

    Temporary reverting changes to sccec_stair_piano.ino for sending to remote

--- sccec_stair_piano.ino
+++ sccec_stair_piano.ino
@@ -11,9 +11,9 @@
 
 const int buttonPin1 = 4;
 const int buttonPin2 = 9;
+
 const int analogPin1 = 2;
 const int IRpin = 6;
-const int outPin1 = 6;
 
 #include "buttonStates.h"
 struct buttonState button1;
@@ -29,16 +29,13 @@ struct buttonState IRbutton;
 void setup()
 {
   pinMode( speakerPin, OUTPUT );
-  pinMode( outPin1, OUTPUT );
   pinMode( buttonPin1, INPUT );
   pinMode( buttonPin2, INPUT );
   pinMode( analogPin1, INPUT );
-  //pinMode( IRpin, INPUT );
+  pinMode( IRpin, INPUT );
   Serial.begin(9600);
 }
 
-int counter = 0;
-int presses = 0;
 void loop()
 {
   updateDigitalButton( &button1, buttonPin1 );
@@ -46,28 +43,16 @@ void loop()
   updateInvAnalogButton( &analog1, analogPin1, 100 );
   updateDigitalButton( &IRbutton, IRpin );
   
-  //Serial.println( analogRead( analogPin1 ) );
+  Serial.println( analogRead( analogPin1 ) );
   
-  if( button1.Pressed )
+  if( analog1.Pressed )
   //if( analog1.Pressed && !bPlaying )
   {
-    //startPlayback( c_data, c_length, 0 );
-    digitalWrite( outPin1, HIGH );
-    presses++;
-    Serial.println( String("Pressed ") + presses );
-    counter = 5;
-  }
-  else if( counter <= 0 )
-  {
-    digitalWrite( outPin1, LOW );
-  }
-  else
-  {
-    --counter;
+    startPlayback( c_data, c_length, 0 );
   }
   if( button2.Pressed )
   {
-    //startPlayback( d_data, d_length, 1 );
-    //Serial.println("Pressed 2");
+    startPlayback( d_data, d_length, 1 );
+    Serial.println("Pressed 2");
   }
 }
